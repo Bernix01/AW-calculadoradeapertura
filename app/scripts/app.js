d4480cd68232399326c32997d088b2dab23d0f87
gbernal096 <gbernal096@gmail.com>
2015-11-05 08:10:23 -0500

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name awContraApp
+ * @description
+ * # awContraApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('awContraApp', [
+    'ngAnimate',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name awContraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the awContraApp
+ */
+angular.module('awContraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
